e9270f46f426b34d51e4071e95c4a11914f2d681
Lonviser <47rex47@mail.ru>
2023-01-22 10:36:48 +0300

    add slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,9 @@
-const humburger = document.querySelector('.humburger');
-const menu = document.querySelector('.menu');
-const closeMenu = document.querySelector('.menu__close');
-
-humburger.addEventListener('click', () =>{
-    menu.classList.add('active');
-})
-
-closeMenu.addEventListener('click', () =>{
-    menu.classList.remove('active');
-})
+var swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
